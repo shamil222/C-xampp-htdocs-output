23a56da8488b9b58892e9c25c45dd29227bbd32d
shamil222 <48852138+shamil222@users.noreply.github.com>
2025-10-19 02:18:09 +0200

    Add files via upload

---
+++ panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-18 23:33:38.131094800 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
